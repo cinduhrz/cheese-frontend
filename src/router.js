0576fcd4ac362183f7b3110bac4ebb9a9f112c73
Cindy Wong <cindywongdev@gmail.com>
2023-01-16 19:15:34 -0500

    added all crud routes to router

--- src/router.js
+++ src/router.js
@@ -5,12 +5,18 @@ import {
     Route
 } from 'react-router-dom'
 import App from './App'
+import Index from './pages/Index'
+import Show from './pages/Show'
 
 // Create router
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App/>}>
-
+            <Route path="cheeses" element={<Index/>}/>
+            <Route path="cheeses/:id" element={<Show/>}/>
+            {/* <Route path="create" action={}/>
+            <Route path="update/:id" action={}/>
+            <Route path="delete/:id" action={}/> */}
         </Route>
     )
 )

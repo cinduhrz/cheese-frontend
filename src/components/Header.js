a65defcf62fed039274828e0fc4b1041fd60dfd9
Cindy Wong <cindywongdev@gmail.com>
2023-01-17 15:28:53 -0500

    addednav and styled it

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Header = (props) => {
-    return (<><h1>Header Component</h1></>)
+    return (<>
+        <nav className="nav">
+            <Link to="/cheeses">
+                <div>Cheeses</div>
+            </Link>
+        </nav>
+    </>)
 }
 
 export default Header
